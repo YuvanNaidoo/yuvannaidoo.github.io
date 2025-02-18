833ad505ff124ca3bb554861d3e58d16a9e2d497
Yuvan Naidoo <yuvannaidoo@gmail.com>
2025-02-18 08:09:09 +0200

    CleanSlate

--- sites/theDiveClub/ui.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("show-signup").addEventListener("click", function() {
-    document.getElementById("login-card").style.display = "none";
-    document.getElementById("signup-card").style.display = "block";
-});
-
-document.getElementById("show-login").addEventListener("click", function() {
-    document.getElementById("signup-card").style.display = "none";
-    document.getElementById("login-card").style.display = "block";
-});
