12729fabf655471bf22705975c5f43f3ca6c06d4
Yuvan Naidoo <yuvannaidoo@gmail.com>
2025-02-19 11:14:38 +0200

    Test

--- sites/theDiveClub/accounts/signUp.js
+++ sites/theDiveClub/accounts/signUp.js
@@ -79,9 +79,9 @@ async function checkForAccessTokenAndRedirect() {
     if (accessToken) {
         await getUserDetails(accessToken);
         const redirectUrl = `https://yuvannaidoo.github.io/sites/theDiveClub/index.html?access_token=${accessToken}`;
-        window.location.href = redirectUrl;
+        //window.location.href = redirectUrl;
     }
 
     
-    console.log('Checking for access token and redirecting');
+    console.log(redirectUrl);
 }
